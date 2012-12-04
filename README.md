93a629dac6866123a84cb2d1cdd08d44cd5fcf10
millermedeiros <miller@millermedeiros.com>
2012-12-04 17:54:00 -0200

    change token getters to normal properties and remove update()

--- README.md
+++ README.md
@@ -5,26 +5,28 @@ API](http://esprima.org/doc/index.html#ast) compatible AST and provides
 different methods for walking the tree recursively.
 
 The main difference about other tools is that it also keeps information about
-tokens and white spaces.
+tokens and white spaces and it is meant to be used to transform the tokens and
+not the string values itself.
 
 
-## Inpiration
+## Inspiration
 
 This module was heavily inspired by
 [node-falafel](https://github.com/substack/node-falafel) but I needed more
 information than what is currently available on falafel (specially about
 tokens, empty lines and white spaces). The amount of changes required to
 introduce the new features and the differences on the concept behind the tool
-justifies a new project.
-
+justified a new project.
 
 
 ## Extra Tokens
 
  * WhiteSpace
-  - Can store multiple white spaces (tabs and line breaks are considered white
-    space). Important if you want to do non-destructive replacements that are
-    white-space sensitive.
+   - Can store multiple white spaces (tabs are considered white space, line
+     breaks not). Important if you want to do non-destructive replacements that
+     are white-space sensitive.
+ * LineBreak
+   - stores info about line breaks;
 
 
 
@@ -33,20 +35,26 @@ justifies a new project.
 Each Node have the following extra properties/methods:
 
   - parent : Node|undefined
-  - update(newStringValue)
   - toString() : string
   - next : Node|undefined
   - prev : Node|undefined
   - depth : Number
-  - getStartToken() : Token
-  - getEndToken() : Token
-  - getPrevToken() : Token|undefined
-  - getNextToken() : Token|undefined
+  - startToken : Token
+  - endToken : Token
+  - prevToken : Token|undefined
+  - nextToken : Token|undefined
   - getTokens() : Array<Token>
+
+Each token also have:
+
+ - prev : Token|undefined
+ - next : Token|undefined
+
 
 The AST root (Program) also have an extra property `nodes`, which contain all
 Nodes that are present inside the `Program.body`. The `nodes` array is *not*
-sorted on any specific way, order of elements is not guaranteed.
+sorted on any specific way, order of elements is not guaranteed. It is used
+internally by the `moonwalk`.
 
 
 
@@ -79,9 +87,6 @@ the root node (`Program`).
 The walk would start from deepest node and would follow the steps from 1-18
 until it reaches the root node. Each node will be traversed only once.
 
-The `getTokens()` method only retuns the orignal tokens, if you `update()` the
-node content it won't reflect the new tokens. Maybe this behavior will change
-in the future since it would be cool to be able to replace the tokens at will.
 
 
 
@@ -90,14 +95,13 @@ in the future since it would be cool to be able to replace the tokens at will.
 ```js
 var walker = require('es-ast-walker');
 
-// you can pass a regular AST or a codeString, codeString will give more
-// details since we can keep the original whiteSpace information
+// it parses a string and instrument the AST with extra properties/methods
 var instrumentedAst = walker.parse(string);
 
 // moonwalk() starts from the deepest nodes and walk the tree backwards
 var result = walker.moonwalk(instrumentedAst, function(node){
     if (node.type == 'ArrayExpression'){
-        node.update( 'fn('+ node.toString() +')' );
+        console.log( node.toString() );
     }
 });
 ```
--- test/parse.spec.js
+++ test/parse.spec.js
@@ -39,11 +39,6 @@ describe('parse', function () {
             expect( returnStatement.toString() ).toEqual( 'return 123 ' );
         });
 
-        it('should add update() method', function () {
-            returnStatement.update('return "foo"');
-            expect( returnStatement.toString() ).toEqual( 'return "foo"' );
-        });
-
         it('should add depth property to nodes', function () {
             expect( program.depth ).toEqual( 0 );
             expect( expressionStatement.depth ).toEqual( 1 );
@@ -70,10 +65,11 @@ describe('parse', function () {
         // });
 
         describe('node.getTokens()', function () {
-            it('should return tokens inside range', function () {
+            it('should return an array of tokens inside range', function () {
                 expect( returnStatement.type ).toEqual( 'ReturnStatement' );
 
                 var tokens = returnStatement.getTokens();
+
                 expect( tokens[0].type ).toEqual( 'Keyword' );
                 expect( tokens[0].value ).toEqual( 'return' );
 
@@ -102,14 +98,14 @@ describe('parse', function () {
             });
         });
 
-        describe('node.getNextToken() / token.next', function () {
+        describe('node.nextToken / token.next', function () {
             it('should return next token', function () {
-                var token = returnStatement.getNextToken();
+                var token = returnStatement.nextToken;
                 expect( token.type ).toEqual( 'WhiteSpace' );
                 expect( token.value ).toEqual( ' ' );
 
                 // just so we make sure token.next works after
-                // node#getNextToken
+                // node#nextToken
                 var comment = token.next;
                 expect( comment.type ).toEqual( 'LineComment' );
                 expect( comment.value ).toEqual( ' foo' );
@@ -123,24 +119,24 @@ describe('parse', function () {
         });
 
 
-        describe('node.getPrevToken()', function () {
+        describe('node.prevToken', function () {
             it('should return previous token', function () {
-                var token = returnStatement.getPrevToken();
+                var token = returnStatement.prevToken;
                 expect( token.type ).toEqual( 'WhiteSpace' );
                 expect( token.value ).toEqual( '  ' );
             });
         });
 
-        describe('node.getEndToken()', function () {
+        describe('node.endToken', function () {
             it('should return last token inside node', function () {
-                var token = returnStatement.getEndToken();
+                var token = returnStatement.endToken;
                 expect( token.value ).toEqual( ';' );
             });
         });
 
-        describe('node.getStartToken()', function () {
+        describe('node.startToken', function () {
             it('should return last token inside node', function () {
-                var token = returnStatement.getStartToken();
+                var token = returnStatement.startToken;
                 expect( token.value ).toEqual( 'return' );
             });
         });
@@ -173,8 +169,6 @@ describe('parse', function () {
             var ast = walker.parse('function foo(){ return "bar"; }');
             var tokens = ast.tokens;
 
-            expect( tokens[0].index ).toEqual( 0 );
-            expect( tokens[1].index ).toEqual( 1 );
             expect( tokens[0].prev ).toBeUndefined();
             expect( tokens[0].next ).toBe( tokens[1] );
             expect( tokens[1].prev ).toBe( tokens[0] );
