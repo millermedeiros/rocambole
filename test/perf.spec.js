ebfb618355c521f105e2899d651ce0f6291fc836
millermedeiros <miller@millermedeiros.com>
2013-01-08 20:18:15 -0200

    improve `moonwalk` performance drastically (110x). see #4
    the sorted insert was very expensive so we used a faster approach.

--- test/perf.spec.js
+++ test/perf.spec.js
@@ -6,27 +6,51 @@ var walker = require('../lib/walker');
 
 var _fs = require('fs');
 
+// since it takes a long time to instrument jQuery we avoid doing it multiple
+// times and cache the result
+var _jqueryAST;
+
 
 describe('performance', function () {
 
-    it('should be fast :P', function () {
-        var file = _fs.readFileSync( __dirname +'/files/crossroads.js', 'utf-8');
-        var startTime = process.hrtime();
-        var ast = walker.parse(file);
-        var diff = process.hrtime(startTime);
-        expect( diff[0] ).to.be.below( 300 );
-        expect( ast.startToken ).not.to.be( undefined );
+    describe('rocambole.parse', function () {
+        it('should be fast :P', function () {
+            var file = _fs.readFileSync( __dirname +'/files/crossroads.js', 'utf-8');
+            var startTime = process.hrtime();
+            var ast = walker.parse(file);
+            var diff = process.hrtime(startTime);
+            expect( diff[0] ).to.be.below( 300 );
+            expect( ast.startToken ).not.to.be( undefined );
+        });
+
+        it('should not take forever to instrument jQuery', function () {
+            var file = _fs.readFileSync( __dirname +'/files/jquery.js', 'utf-8');
+            var startTime = process.hrtime();
+            var ast = walker.parse(file);
+            var diff = process.hrtime(startTime);
+            expect( diff[0] ).to.be.below( 10000 );
+            expect( ast.startToken ).not.to.be( undefined );
+            _jqueryAST = ast;
+        });
     });
 
-    it('should not take forever to instrument jQuery', function () {
-        var file = _fs.readFileSync( __dirname +'/files/jquery.js', 'utf-8');
-        var startTime = process.hrtime();
-        var ast = walker.parse(file);
-        var diff = process.hrtime(startTime);
-        expect( diff[0] ).to.be.below( 10000 );
-        expect( ast.startToken ).not.to.be( undefined );
+    describe('rocambole.moonwalk', function () {
+        it('should not take forever to loop over jQuery nodes', function () {
+            if (! _jqueryAST) {
+                var file = _fs.readFileSync( __dirname +'/files/jquery.js', 'utf-8');
+                _jqueryAST = walker.parse(file);
+            }
+            var startTime = process.hrtime();
+            var count = 0;
+            walker.moonwalk(_jqueryAST, function(node){
+                if (!node) throw new Error('node should not be undefined');
+                count += 1;
+            });
+            var diff = process.hrtime(startTime);
+            expect( diff[0] ).to.be.below( 200 );
+            expect( count ).to.be.above( 20000 );
+        });
     });
-
 
 });
 
